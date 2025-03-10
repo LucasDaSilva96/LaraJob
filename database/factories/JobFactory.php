687a7645b82a67b249e93f850a1696af7d1bed33
LucasDaSilva96 <dasilvajunior881@gmail.com>
2025-03-10 21:31:00 +0100

    Created factories

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->jobTitle(),
+            'location' => $this->faker->city(),
+            'salary' => $this->faker->randomFloat(2, 1000, 9000),
+            'employer_id' => \App\Models\Employer::factory()->create()->id,
+            'schedule' => $this->faker->randomElement(['Full-time', 'Part-time', 'Contract', 'Temporary', 'Internship']),
+            'url' => $this->faker->url(),
+            'featured' => $this->faker->boolean(),
+        ];
+    }
+}
---
+++ database/migrations/2025_03_10_200157_create_employers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->string('logo')->nullable(false);
+            $table->foreignIdFor(User::class)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employers');
+    }
+};
